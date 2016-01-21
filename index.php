5231ef67eaa4c18da6a53138202c0847457e21f8
Aman Kaushal <kaushalaman02@gmail.com>
2016-01-22 03:00:41 +0530

    animate css added

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
      <link href="css/custom.css" rel="stylesheet">
      <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
      <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+     <link href='https://www.lazylad.com/assets/css/animate.css' rel='stylesheet' type='text/css'>
     <!-- Custom Fonts -->
 
     <!--<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>-->
